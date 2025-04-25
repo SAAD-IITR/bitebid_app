700cab612cb3116169789e4f808a9fd5decb2284
SABIR <sabir.gusknp2022@gmail.com>
2025-04-26 04:15:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:bitebid_app/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/otp_verification_screen.dart';
+import 'utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +20,15 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SplashScreen(),
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (ctx) => const SplashScreen(),
+        '/login':  (ctx) => const LoginScreen(),
+        '/signup': (ctx) => const SignupScreen(),
+        '/otp':
+            (context) =>
+                const OtpVerificationScreen(phoneNumber: '+91 1234567890'),
+      },
     );
   }
 }
